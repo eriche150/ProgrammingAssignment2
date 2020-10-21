87fa031aa72e67cc6446f216386f6a0df7723ad3
eric he <erhe@health.ucsd.edu>
2020-10-21 10:58:43 -0700

    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
-        set <- function(y) {
+        inv <- NULL #undefined value 
+        set <- function(y) { #embedded another function within makeCacheMatrix
                 x <<- y
                 inv <<- NULL
         }
         get <- function() {x} 
-        setInverse <- function(inverse) {inv <<- inverse}
+        setInverse <- function(inverse) {inv <<- inverse} #this is the value of the inverse matrix
         getInverse <- function() {inv}
         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
         
@@ -13,8 +13,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x,...) {
         inv <- x$getInverse() 
-        if (!is.null(inv)) {
-                messeage("getting cached data")
+        if (!is.null(inv)) { #if inv is NOT NULL 
+                message("getting cached data")
                 return(inv)
         }
         mat <- x$get()
